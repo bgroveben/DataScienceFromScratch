cd8d5e596013441fa266a0db1bc60400447f0d90
bgroveben <bgroveben@gmail.com>
2017-01-05 19:23:44 -0800

    Write function definitions for Chapter on Multiple Regression

--- statistics.py
+++ statistics.py
@@ -14,6 +14,34 @@ def de_mean(x):
     return [x_i - x_bar for x_i in x]
 
 
+def median(v):
+    """ finds the 'middle-most' value of v """
+    n = len(v)
+    sorted_v = sorted(v)
+    midpoint = n // 2
+    if n % 2 == 1:
+        # if odd, return the middle value
+        return sorted_v[midpoint]
+    else:
+        # if even, return the average of the middle values
+        lo = midpoint - 1
+        hi = midpoint
+        return (sorted_v[lo] + sorted_v[hi]) / 2
+
+
+def quantile(x, p):
+    """ returns the pth-percentile value in x """
+    p_index = int(p * len(x))
+    return sorted(x)[p_index]
+
+
+def mode(x):
+    """ returns a list, might be ore than one mode """
+    counts = Counter(x)
+    max_count = max(counts.values())
+    return [x_i for x_i, count in counts.iteritems() if count == max_count]
+
+
 def variance(x):
     """ assumes x has at least 2 elements """
     n = len(x)
